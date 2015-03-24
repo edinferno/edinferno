4c2e14b4719aed5474353b81f84decc2385378ca
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-24 18:09:29 +0000

    Extract helper functions to separate header
    The functions dump() and fatal() now can be reused by other codes.
    
    Change-Id: I64def700ef4b1d07c45022407c7039f67ae9502b

--- pippi/server.c
+++ pippi/server.c
@@ -5,38 +5,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "network.h"
+
 #define PORT 7890  // The port users will be connecting to
-
-void fatal(char const * const msg) {
-    fprintf(stderr, "%s\n", msg);
-    exit(0);
-}
-
-void dump(const unsigned char *data_buffer, const unsigned int length) {
-    unsigned char byte;
-    unsigned int i, j;
-    for (i = 0; i < length; ++i) {
-        byte = data_buffer[i];
-        printf("%02x ", byte);  // Display byte in hex.
-        // Every sixteen bytes, or at the end of the buffer:
-        if (((i % 16) == 15) || (i == length - 1)) {
-            // Add padding at the end of the last line
-            for (j = 0; j < 15 - (i % 16); ++j) {
-                printf("   ");
-            }
-            printf("| ");
-            for (j = (i - (i % 16)); j <= i; ++j) {  // Display printable bytes
-                byte = data_buffer[j];
-                if ((byte > 31) && (byte < 127)) {
-                    printf("%c", byte);
-                } else {
-                    printf(".");
-                }
-            }
-            printf("\n");  // End of the dump line
-        }
-    }
-}
 
 int main(void) {
     int sockfd, new_sockfd;  // Listen on sock_fd, new connection on new_fd
