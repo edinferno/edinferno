de923f708593a94d4b0823b23bc688ab65d1bbd6
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-24 15:59:37 +0000

    Update byte array dump function
    The new function shows the byte array in a standard form, similarly to system
    tools like xxd. Here is an example of how that looks like:
    
    [server]
    > ./a.out
    
    [client]
    > telnet 127.0.0.1 7890
    Trying 127.0.0.1...
    Connected to 127.0.0.1.
    Escape character is '^]'.
    Hello, world!
    
    [server]
    server: got connection from 127.0.0.1 port 38281
    
    [client - enter the following]
    I wonder what would happen if I enter this...
    
    [server]
    RECV: 2 bytes
    0d 0a                                           | ..
    RECV: 47 bytes
    49 20 77 6f 6e 64 65 72 20 77 68 61 74 20 77 6f | I wonder what wo
    75 6c 64 20 68 61 70 70 65 6e 20 69 66 20 49 20 | uld happen if I
    65 6e 74 65 72 20 74 68 69 73 2e 2e 2e 0d 0a    | enter this.....
    
    This would allow easier lowest-level debug of communications.
    
    Change-Id: I7c7cb56e1052d47969e4f1e30cfc9f6bfbba66ae

--- pippi/server.c
+++ pippi/server.c
@@ -13,9 +13,28 @@ void fatal(char const * const msg) {
 }
 
 void dump(const unsigned char *data_buffer, const unsigned int length) {
-    int i;
+    unsigned char byte;
+    unsigned int i, j;
     for (i = 0; i < length; ++i) {
-        printf("%c", data_buffer[i]);
+        byte = data_buffer[i];
+        printf("%02x ", byte);  // Display byte in hex.
+        // Every sixteen bytes, or at the end of the buffer:
+        if (((i % 16) == 15) || (i == length - 1)) {
+            // Add padding at the end of the last line
+            for (j = 0; j < 15 - (i % 16); ++j) {
+                printf("   ");
+            }
+            printf("| ");
+            for (j = (i - (i % 16)); j <= i; ++j) {  // Display printable bytes
+                byte = data_buffer[j];
+                if ((byte > 31) && (byte < 127)) {
+                    printf("%c", byte);
+                } else {
+                    printf(".");
+                }
+            }
+            printf("\n");  // End of the dump line
+        }
     }
 }
 
