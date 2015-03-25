b9b6dac6c4f69da73a6171b447c9bede713bd68c
Stan Manilov <stanislav.manilov@gmail.com>
2015-03-25 11:16:48 +0000

    Add comms interface as per Design Doc
    The Communication module implements the design described in the
    Communications API design doc. This consists of a class encapsulating
    the implementation and exposing the desired interface functions.
    
    The next steps are to:
     - write a wrapper in ROS to integrate in edante
     - write implementation ... this would also use ROS
    
    The concept of how we can have a clean interface and use ROS at the same
    time needs to be clarified.
    
    Change-Id: I658ae62d9598009dc3c58dc07611479a763ef635

---
+++ edante/comms/include/Space.h
@@ -0,0 +1,35 @@
+/// Defines structures representing space. 
+#ifndef EDANTE_SPACE_H
+#define EDANTE_SPACE_H
+
+/// Specifies a point in 2D space.
+class Point2D {
+public:
+    /// Returns the x coordinate.
+    double getX();
+    /// Returns the y coordinate.
+    double getY();
+};
+
+
+/// Specifies a vector in 2D space.
+class Vector2D {
+public:
+    /// Returns the magnitude of the vector in the x direction.
+    double getDX();
+    /// Returns the magnitude of the vector in the y direction.
+    double getDY();
+};
+
+/// Specifies a point and orientation in 2D space.
+class Pose2D {
+public:
+    /// Returns the x coordinate.
+    double getX();
+    /// Returns the y coordinate.
+    double getY();
+    /// Returns the orientation θ.
+    double getTheta();
+};
+
+#endif
---
+++ edante/comms/include/Time.h
@@ -0,0 +1,14 @@
+/// Defines structures representing time. 
+#ifndef EDANTE_TIME_H
+#define EDANTE_TIME_H
+
+#include <stdint.h>
+
+/// Specifies a point in 1D time.
+class Timestamp {
+public:
+    /// Returns the timestamp value.
+    uint64_t getValue();
+};
+
+#endif
