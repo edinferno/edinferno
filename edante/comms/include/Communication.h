a4c1c6f499d89c6b5d75fe3b54ff6c7b20a9ff86
KamenB <kbrest@abv.bg>
2015-03-29 13:43:39 +0100

    Extend Comms; Add a test; Add SPLSTDM;
    Add getSTDM method to the Communications class which returns a copy of a
    local variable. Implement the setters of the class.
    
    Add the SPL standard message class from SPL's official site.
    Override the output stream operator of the SPLSTDM for debugging
    purposes.
    
    Add a test file for the Communications class to the project in the tests
    folder. It tests the setters of the class as well as the overriden
    stream operator.
    
    Change the Space and Time classes to structs.
    
    Change-Id: I6cddb19187e97edbb2ff0dc400cdd7ebd54ee48f

--- edante/comms/include/Communication.h
+++ edante/comms/include/Communication.h
@@ -30,7 +30,12 @@
 
 #include "Mental.h"
 
+#include "SPLStandardMessage.h"
+
 class Communication {
+private:
+    SPLStandardMessage stdmsg;
+
 public:
     /// Sets the pose of the robot on the field at the given point in time.
     ///  - The pose is given in absolute coordinates as (x, y, θ)
@@ -62,6 +67,68 @@ public:
     /// Sets the current intention of the robot and the time at which the
     /// decision was made.
     void setIntention(Intention intention, Timestamp at);
+    /// Gives the latest constructed standard message.
+    SPLStandardMessage getStandardMessage();
 };
+
+void Communication::setPose(Pose2D pose, Timestamp at) {
+    stdmsg.pose[0] = pose.x;
+    stdmsg.pose[1] = pose.y;
+    stdmsg.pose[2] = pose.theta;
+}
+
+void Communication::setWalkingTo(Point2D point, Timestamp at) {
+    stdmsg.walkingTo[0] = point.x;
+    stdmsg.walkingTo[1] = point.y;
+}
+
+void Communication::unsetWalkingTo(Timestamp at) {
+    /// If we don't want to move, give our position as our walking target.
+    stdmsg.walkingTo[0] = standardMessage.pose[0];
+    stdmsg.walkingTo[1] = standardMessage.pose[1];
+}
+
+void Communication::setShootingTo(Point2D point, Timestamp at) {
+    stdmsg.shootingTo[0] = point.x;
+    stdmsg.shootingTo[1] = point.y;
+}
+
+void Communication::unsetShootingTo(Timestamp at) {
+    /// If we don't want to shoot, give our position as our shooting target.
+    stdmsg.shootingTo[0] = standardMessage.pose[0];
+    stdmsg.shootingTo[1] = standardMessage.pose[1];
+}
+
+void Communication::setBallPosition(Point2D point, Timestamp at) {
+    stdmsg.ball[0] = point.x;
+    stdmsg.ball[1] = point.y;
+}
+
+void Communication::setBallVelocity(Vector2D velocity, Timestamp at) {
+    stdmsg.ballVel[0] = velocity.dx;
+    stdmsg.ballVel[1] = velocity.dy;
+}
+
+void Communication::setIntention(Intention intention, Timestamp at) {
+    stdmsg.intention = (int8_t) intention;
+}
+
+SPLStandardMessage Communication::getStandardMessage() {
+
+    /// TODO: Set these
+    /// standardMessage.fallen = ;
+
+    /// standardMessage.ballAge = ;
+
+    /// standardMessage.suggestion = ;
+
+    /// standardMessage.currentPositionConfidence = ;
+    /// standardMessage.currentSideConfidence = ;
+
+    /// standardMessage.numOfDataBytes = ;
+    /// standardMessage.data = ;
+
+    return stdmsg;
+}
 
 #endif
