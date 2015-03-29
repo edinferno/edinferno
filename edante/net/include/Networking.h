edee80263fd559afd7b46485bfe903c20c02eb46
Bence Karpati <s1345679@sms.ed.ac.uk>
2015-03-29 14:02:18 +0100

    Add Networking.h created according to the documentation
    Change-Id: I3b77daaa6da50d84acfd33ec1b478dfd48483771

---
+++ edante/net/include/Networking.h
@@ -0,0 +1,15 @@
+/// Implementation of the Communication module from the Architecure Diagram.
+/// The SPL Standard Message is created by the Commuication Module
+/// and then sent to the Communication module via ROS.
+
+#ifndef EDANTE_NET_H
+#define EDANTE_NET_H
+class Networking {
+public:
+
+    /// Sends the specified standard message to all teammates.
+    void sendStandardMessage(SPLStandardMessage splMsg);
+
+    /// Receives a standard message from a teammate and returns it.
+    void receiveStandardMessage(SPLStandardMessage splMsg);
+};
