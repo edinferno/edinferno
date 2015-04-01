6628433f05bbc1ac27d79576e46b2e14211bd4ab
Bence Karpati <s1345679@sms.ed.ac.uk>
2015-04-01 17:12:20 +0100

    Update Networking interface; create test file
    Update Networking.h to include a constructor and data fields for its
    functions. It currently also includes the number of team mates,
    their addresses and ports as constants. Add missing #endif at
    the end. And also add return value to receiveStandardMessage and remove
    its argument.
    
    Add Networking.cpp, which currently implements the constructor of
    the Networking class and the sendStandardMessage function of
    Networking.h.
    
    Add SPLStandardMessage.h, copy of SPLStandardMessage from
    the comms branch.
    
    Add NetworkingTest.cpp, which tests the Networking interface,
    using test data.
    
    Add Makefile for testing to make it easier.
    
    Change-Id: I0fdd82d84737b8a5d7bcaec098bdd6925e2f4299

--- edante/net/include/Networking.h
+++ edante/net/include/Networking.h
@@ -4,12 +4,44 @@
 
 #ifndef EDANTE_NET_H
 #define EDANTE_NET_H
+
+#include <netinet/in.h>
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+
+#include "SPLStandardMessage.h"
+
+const int            NUM_OF_TEAMMATES = 4;
+const vector<string> TEAMMATE_ADDRESS = {
+                         "127.0.0.1",
+                         "127.0.0.1",
+                         "127.0.0.1",
+                         "127.0.0.1"
+                     };
+const vector<int>    TEAMMATE_PORT    = {
+                         7890,
+                         7891,
+                         7892,
+                         7893
+                     };
+
 class Networking {
+
+    int send_socket_fd;
+    vector<struct sockaddr_in> teammate_addresses;
+
 public:
+    Networking();
 
     /// Sends the specified standard message to all teammates.
     void sendStandardMessage(SPLStandardMessage splMsg);
 
     /// Receives a standard message from a teammate and returns it.
-    void receiveStandardMessage(SPLStandardMessage splMsg);
+    SPLStandardMessage receiveStandardMessage();
+    // Possible optimizatio: Try to use const & and see if performance is
+    // better.
 };
+
+#endif
