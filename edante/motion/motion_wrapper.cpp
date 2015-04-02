0b34188b6b0aa01776ff7123c4e588e73eed10cf
Giuseppe Li <beppe712@hotmail.it>
2015-04-02 14:26:11 +0100

    motion_wrapper v1.1
    Added the methods from the Joint Control API.
    Amended some possible syntactical errors from v1.0

--- edante/motion/motion_wrapper.cpp
+++ edante/motion/motion_wrapper.cpp
@@ -17,11 +17,11 @@ void rest(AL::ALMotionproxy& motion){
     motion.rest();
 }
 
-void setStiffness(AL::ALMotionproxy& motion, const std::vector<std::string> names, const std::vector<double>stiffnesses){
+void setStiffness(AL::ALMotionproxy& motion, const std::vector<std::string>& names, const std::vector<double>& stiffnesses){
     motion.setStiffness(ALValue(names), ALValue(stiffnesses));
 }
 
-std::vector<float> getStiffnesses(AL::ALMotionproxy& motion, const std::vector<std::string> names){
+std::vector<float> getStiffnesses(AL::ALMotionproxy& motion, const std::vector<std::string>& names){
     return ToFloatArray(motion.getStiffnesses(ALValue(names)));
     
     
@@ -41,3 +41,32 @@ int main(int argc, char **argv){
     }
 
 }
+
+void angleInterpolation(AL::ALMotionproxy& motion, const std::vector<std::string>& names, const std::vector<float>& angleLists, const std::vector<float>& timeLists, const bool& isAbsolute){
+    motion.angleInterpolation(ALValue(names), ALValue(angleLists), ALValue(timeLists), bool(isAbsolute));
+}
+
+void angleInterpolationWithSpeed(AL::ALMotionproxy& motion, const std::vector<std::string>& names, const std::vector<float>& targetAngles, const float& maxSpeedFraction){
+    motion.angleInterpolationWithSpeed(ALValue(names), ALValue(targetAngles), float(maxSpeedFraction));
+}
+
+void angleInterpolationBezier(AL::ALMotionproxy& motion, const std::vector<std::string>& jointNames, const std::vector<float>& times, const std::vector<std::vector<float angle, std::vector<int interpolationType, float dAngle, float dTime> Handle1, std::vector<int interpolationType, float dAngle, float dTime> Handle2>>& controlPoints){
+    motion.angleInterpolationBezier(std::vector<std::string>(jointNames), ALValue(times), ALValue(controlPoints));
+}
+
+void setAngles(AL::ALMotionproxy& motion, const std::vector<std::string>& names, const std::vector<float>& angles, const float& fractionMaxSpeed){
+    motion.setAngles(ALValue(names), ALValue(angles), float(fractionMaxSpeed));
+}
+
+void changeAngles(AL::ALMotionproxy& motion, const std::vector<std::string>& names, const std::vector<float>& changes, const float& fractionMaxSpeed){
+    motion.changeAngles(ALValue(names), ALValue(changes), float(fractionMaxSpeed));
+}
+
+std::vector<float> getAngles(AL::ALMotionproxy& motion, const std::vector<std::string>& names, const bool& useSensors){
+    return motion.getAngles(ALValue(names), bool(useSensors));
+}
+
+void closeHand(AL::ALMotionproxy& motion, const std::string& handName){
+    motion.closeHand(std::string(handName));
+}
+
