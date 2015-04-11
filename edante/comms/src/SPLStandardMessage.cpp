7a728c92ef12656d9258cfe2354f2231ee046fad
Stan Manilov <stanislav.manilov@gmail.com>
2015-04-11 12:27:30 +0100

    Add missing file
    Forgot to add this as part of previous commit. Apparantly, reviewers
    didn't notice this.
    
    Change-Id: I20a89e015d4ac4158234b67ff0ea46e89d2bb38b

---
+++ edante/comms/src/SPLStandardMessage.cpp
@@ -0,0 +1,17 @@
+#include "SPLStandardMessage.h"
+
+comms::SPLStandardMessage convertToROS(SPLStandardMessage msg) {
+    comms::SPLStandardMessage msg_;
+
+    // TODO: fill msg_ from msg
+
+    return msg_;
+}
+
+SPLStandardMessage convertFromROS(comms::SPLStandardMessage msg) {
+    SPLStandardMessage msg_;
+
+    // TODO: fill msg_ from msg
+
+    return msg_;
+}
