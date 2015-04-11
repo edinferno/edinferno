8dea08f9689a23f4b62824c196bdddfc200a07ae
Bence Karpati <s1345679@sms.ed.ac.uk>
2015-04-11 14:32:58 +0100

    Split Networking into Receiver and Sender
    Split Networking.cpp and Networking.h into two parts according to
    the sending and receiving functions and so create NetReceiver.cpp and
    NetSender.cpp and their header files.
    
    Split NetWrapper into two parts according to the sending and receiving
    functions to create NetSenderWrapper.cpp and NetReceiverWrapper.cpp.
    These functions are currently boilerplate functions.
    
    Update test files and the makefile to work with the changes detailed
    above.
    
    Change-Id: I07a984781c8ec396d132f6031c0ea170c77198b3

--- edante/net/src/NetSenderWrapper.cpp
+++ edante/net/src/NetSenderWrapper.cpp
@@ -1,7 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-#include "Networking.h"
+#include "NetSender.h"
 #include "SPLStandardMessage.h"
 
 typedef std_msgs::String OurMsg;
@@ -12,9 +12,9 @@ OurMsg convert() {
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "net");
+    ros::init(argc, argv, "net_sender");
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<OurMsg>("net/spl_std_msg", 1000);
+    ros::Publisher pub = n.advertise<OurMsg>("net_out/spl_std_msg", 1000);
     ros::Rate loopRate(5);
     while (ros::ok()) {
         pub.publish(convert());
