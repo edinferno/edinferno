0f6fbc9e1494f85e5f7bbc335014d559647fcbf2
Svetlin Penkov <sv.penkov@gmial.com>
2015-04-15 17:15:53 +0100

    Add a ROS Installation Script
    The install_ros_opennao.sh script installs ROS on a new
    OpenNAO 1.14 distribution using the openrobots repository.

---
+++ tools/install_ros_opennao.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# This scripts installs ROS on an OpenNAO distribution
+# using the openrobots repository
+
+# Add yourself to the group of sudo-ers
+su -c"echo 'nao     ALL=(ALL) ALL' >> /etc/sudoers"
+
+# Download a boostraping script which downloads some essential tools 
+curl -k -s https://chili-research.epfl.ch/ros4nao/bootstrap.sh | sh
+ 
+# Set changes
+sudo emerge log4cxx netifaces pyyaml poco apr apr-util
+# Apply changes
+sudo emerge log4cxx netifaces pyyaml poco apr apr-util
+ 
+ # Install nao-robot from the openrobots repo
+sudo /opt/openrobots/bin/robotpkgin install nao-robot
+
+# Install ros-comm from the openrobots repo, which is 
+# currently a broken dependency
+sudo /opt/openrobots/bin/robotpkgin install ros-comm
+ 
+source /opt/openrobots/etc/ros/setup.sh
+ 
+roscore
