dc7e7fc5e3f0ce79dddacf1d0d7afd739876683c
KamenB <kbrest@abv.bg>
2015-04-15 15:32:06 +0100

    Move message converters from CommsWrapper to STDM
    Move function implementation of converters between SPLSTDM and
    ROS SPLSTDM from CommsWrapper.cpp to SPLStandardMessage.cpp.
    
    Move ostream operator overload implementation from SPLStandardMessage.h
    to SPLStandardMessage.cpp. This operator is used for debbuging.
    
    Change-Id: I1ed18847e460964f56d52b8918c85c3445c2f63a

--- edante/comms/src/SPLStandardMessage.cpp
+++ edante/comms/src/SPLStandardMessage.cpp
@@ -1,9 +1,32 @@
 #include "SPLStandardMessage.h"
 
-comms::SPLStandardMessage convertToROS(SPLStandardMessage msg) {
+comms::SPLStandardMessage convertToROS(const SPLStandardMessage& msg) {
     comms::SPLStandardMessage msg_;
 
-    // TODO: fill msg_ from msg
+    msg_.pose[0] = msg.pose[0];
+    msg_.pose[1] = msg.pose[1];
+    msg_.pose[2] = msg.pose[2];
+    msg_.walkingTo[0] = msg.walkingTo[0];
+    msg_.walkingTo[1] = msg.walkingTo[1];
+    msg_.shootingTo[0] = msg.shootingTo[0];
+    msg_.shootingTo[1] = msg.shootingTo[1];
+    msg_.ballAge = msg.ballAge;
+    msg_.ball[0] = msg.ball[0];
+    msg_.ball[1] = msg.ball[1];
+    msg_.ballVel[0] = msg.ballVel[0];
+    msg_.ballVel[1] = msg.ballVel[1];
+    for (int8_t i = 0; i < SPL_STANDARD_MESSAGE_MAX_NUM_OF_PLAYERS; i++) {
+        msg_.suggestion[i] = msg.suggestion[i];
+    }
+    msg_.intention = msg.intention;
+    msg_.averageWalkSpeed = msg.averageWalkSpeed;
+    msg_.maxKickDistance = msg.maxKickDistance;
+    msg_.currentPositionConfidence = msg.currentPositionConfidence;
+    msg_.currentSideConfidence = msg.currentSideConfidence;
+    msg_.numOfDataBytes = msg.numOfDataBytes;
+    for (int8_t i = 0; i < msg.numOfDataBytes; i++) {
+        msg_.data[i] = msg.data[i];
+    }
 
     return msg_;
 }
@@ -11,7 +34,61 @@ comms::SPLStandardMessage convertToROS(SPLStandardMessage msg) {
 SPLStandardMessage convertFromROS(comms::SPLStandardMessage msg) {
     SPLStandardMessage msg_;
 
-    // TODO: fill msg_ from msg
+    msg_.pose[0] = msg.pose[0];
+    msg_.pose[1] = msg.pose[1];
+    msg_.pose[2] = msg.pose[2];
+    msg_.walkingTo[0] = msg.walkingTo[0];
+    msg_.walkingTo[1] = msg.walkingTo[1];
+    msg_.shootingTo[0] = msg.shootingTo[0];
+    msg_.shootingTo[1] = msg.shootingTo[1];
+    msg_.ballAge = msg.ballAge;
+    msg_.ball[0] = msg.ball[0];
+    msg_.ball[1] = msg.ball[1];
+    msg_.ballVel[0] = msg.ballVel[0];
+    msg_.ballVel[1] = msg.ballVel[1];
+    for (int8_t i = 0; i < SPL_STANDARD_MESSAGE_MAX_NUM_OF_PLAYERS; i++) {
+        msg_.suggestion[i] = msg.suggestion[i];
+    }
+    msg_.intention = msg.intention;
+    msg_.averageWalkSpeed = msg.averageWalkSpeed;
+    msg_.maxKickDistance = msg.maxKickDistance;
+    msg_.currentPositionConfidence = msg.currentPositionConfidence;
+    msg_.currentSideConfidence = msg.currentSideConfidence;
+    msg_.numOfDataBytes = msg.numOfDataBytes;
+    for (int8_t i = 0; i < msg.numOfDataBytes; i++) {
+        msg_.data[i] = msg.data[i];
+    }
 
     return msg_;
+}
+
+std::ostream& operator<<(std::ostream& stream, const SPLStandardMessage& obj) {
+    stream << "Pose: " << obj.pose[0] << " " << obj.pose[1] << " " << obj.pose[2] << std::endl;
+    stream << "Walking To: " << obj.walkingTo[0] << " " << obj.walkingTo[1] << std::endl;
+    stream << "Shooting To: " << obj.shootingTo[0] << " " << obj.shootingTo[1] << std::endl;
+
+    stream << "Ball Age: " << obj.ballAge << std::endl;
+    stream << "Ball Position: " << obj.ball[0] << " " << obj.ball[1] << std::endl;
+    stream << "Ball Velocity: " << obj.ballVel[0] << " " << obj.ballVel[1] << std::endl;
+
+    stream << "Suggestions: ";
+    for (int i = 0; i < SPL_STANDARD_MESSAGE_MAX_NUM_OF_PLAYERS; i++) {
+        stream << (int)obj.suggestion[i] << " ";
+    }
+    stream << std::endl;
+    stream << "Intention: " << (int)obj.intention << std::endl;
+
+    stream << "Average Walking Speed: " << obj.averageWalkSpeed << std::endl;
+    stream << "Max Kick Distance: " << obj.maxKickDistance << std::endl;
+
+    stream << "Current Position Confidence: " << (int)obj.currentPositionConfidence << std::endl;
+    stream << "Current Side Confidence: " << (int)obj.currentSideConfidence << std::endl;
+
+    stream << "Number of Data Bytes: " << obj.numOfDataBytes << std::endl;
+    stream << "Data: " ;
+    for (int i = 0; i < obj.numOfDataBytes; i++) {
+      stream << std::hex << (int)obj.data[i] << " ";
+    }
+    stream << std::endl;
+    return stream;
 }
