ba9cc524f38cb2cfe9ab5b05aea61cb848ea8928
Svetlin Penkov <sv.penkov@gmial.com>
2015-04-22 17:45:28 +0100

    Imporve the ROS installation script
    Improve the ROS installation script to provide necessary
    instructions. Add a check if authentication has failed
    in order to terminate the script immediately.

--- tools/install_ros_opennao.sh
+++ tools/install_ros_opennao.sh
@@ -3,18 +3,50 @@
 # This scripts installs ROS on an OpenNAO distribution
 # using the openrobots repository
 
+# Define a constant for the collor
+yellow='\e[1;33m'
+
+echo -e "${yellow}==============================================="
+echo -e "${yellow}  INSTRUCTIONS: You are about to install ROS"
+echo -e "${yellow}  on a fresh OpenNAO distribution. There will" 
+echo -e "${yellow}  be several instruction blocks like this one."
+echo -e "${yellow}  Please read each ENTIRE instruction block " 
+echo -e "${yellow}  before proceeding (unlike Alex)." 
+echo -e "${yellow}==============================================="
+echo -e "\033[0m"
+
+echo -e "${yellow}==============================================="
+echo -e "${yellow}  INSTRUCTIONS: Enter the superuser password "
+echo -e "${yellow}  (root) and then nao user password (nao)."
+echo -e "${yellow}==============================================="
+echo -e "\033[0m"
+
 # Add yourself to the group of sudo-ers
 su -c"echo 'nao     ALL=(ALL) ALL' >> /etc/sudoers"
+# Check if authentication was not successfull
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
+
 
 # Download a boostraping script which downloads some essential tools 
 curl -k -s https://chili-research.epfl.ch/ros4nao/bootstrap.sh | sh
 
-# Set changes
+# Attempt to install packages and write the new portage settings
+# files if necessary.
 sudo emerge --autounmask-write log4cxx netifaces pyyaml poco apr apr-util
 
-etc-update
-# Apply changes
-sudo emerge log4cxx netifaces pyyaml poco apr apr-util
+echo -e "${yellow}==============================================="
+echo -e "${yellow}  INSTRUCTIONS: Select '-3' and then type 'y'"
+echo -e "${yellow}  for every prompt after that."
+echo -e "${yellow}==============================================="
+echo -e "\033[0m"
+
+# Update the portage settings file
+sudo etc-update
+
+# Finally install the packages
+sudo emerge -G log4cxx netifaces pyyaml poco apr apr-util
 
 # Install nao-robot from the openrobots repo
 sudo /opt/openrobots/bin/robotpkgin install nao-robot
@@ -23,6 +55,11 @@ sudo /opt/openrobots/bin/robotpkgin install nao-robot
 # currently a broken dependency
 sudo /opt/openrobots/bin/robotpkgin install ros-comm
  
-source /opt/openrobots/etc/ros/setup.sh
- 
-roscore
+# Set the ROS setup.sh to be called automatically at logon
+echo "source /opt/openrobots/etc/ros/setup.sh" >> ~/.bash_profile
+
+echo -e "${yellow}==============================================="
+echo -e "${yellow} INSTRUCTIONS: Add you hostname to /etc/hosts"
+echo -e "${yellow} and restart your computer to start using ROS."
+echo -e "${yellow}==============================================="
+echo -e "\033[0m"
