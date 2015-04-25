7ce4084e0eb1c6cfdcc019ea2ea43ceb30d3e5eb
GreatAlexander <alex.bordallo@gmail.com>
2015-04-25 21:09:02 +0000

    Adds the angleInterpolation service, service test file, floatList msg
     - Figured out the fricking AL::Value and fricking AL::Value::arrays
     - Setup a float32List.msg for sending a list of lists of floats
     - srv_test.cpp is used for testing services with complex structures
     - anglInterp service unpacks srv structure into fricking AL::Values

--- edante/motion/src/joint_control.cpp
+++ edante/motion/src/joint_control.cpp
@@ -17,22 +17,59 @@ Joint_Control::Joint_Control()
   // INFO("Setting up Joint Control subscribers" << std::endl);
 
   INFO("Setting up Joint Control services" << std::endl);
-  srv_set_angles_ = nh_->advertiseService("motion/setAngles",
-                                          &Joint_Control::setAngles, this);
-  srv_change_angles_ = nh_->advertiseService("motion/changeAngles",
-                                            &Joint_Control::changeAngles, this);
-  srv_get_angles_ = nh_->advertiseService("motion/getAngles",
-                                          &Joint_Control::getAngles, this);
-  srv_close_hand_ = nh_->advertiseService("motion/closeHand",
-                                           &Joint_Control::closeHand, this);
-  srv_open_hand_ = nh_->advertiseService("motion/openHand",
-                                         &Joint_Control::openHand, this);
+
+  srv_angle_interp_ = nh_->advertiseService(
+                  "motion/angleInterp", &Joint_Control::angleInterp, this);
+  srv_angle_interp_speed_ = nh_->advertiseService(
+                  "motion/angleInterpSpeed", &Joint_Control::angleInterpSpeed, this);
+  srv_set_angles_ = nh_->advertiseService(
+                  "motion/setAngles", &Joint_Control::setAngles, this);
+  srv_change_angles_ = nh_->advertiseService(
+                  "motion/changeAngles", &Joint_Control::changeAngles, this);
+  srv_get_angles_ = nh_->advertiseService(
+                  "motion/getAngles", &Joint_Control::getAngles, this);
+  srv_close_hand_ = nh_->advertiseService(
+                  "motion/closeHand", &Joint_Control::closeHand, this);
+  srv_open_hand_ = nh_->advertiseService(
+                  "motion/openHand", &Joint_Control::openHand, this);
 }
 
 Joint_Control::~Joint_Control()
 {
   ros::shutdown();
   delete nh_;
+}
+
+bool Joint_Control::angleInterp(motion::angleInterp::Request &req,
+                                motion::angleInterp::Response &res)
+{
+  int s = req.names.size();
+  AL::ALValue names = req.names;
+
+  AL::ALValue angleLists;
+  angleLists.arraySetSize(s);
+  AL::ALValue timeLists;
+  timeLists.arraySetSize(s);
+
+  for(unsigned i = 0; i < s; ++i) {
+    angleLists[i] = req.angleLists[i].floatList;
+    timeLists[i] = req.timeLists[i].floatList;
+  }
+
+  try{
+    mProxy_->angleInterpolation(names, angleLists,
+                                timeLists, true);
+    res.res = true;
+  }
+  catch (const std::exception& e){
+    res.res = false;
+  }
+  return true;
+}
+bool Joint_Control::angleInterpSpeed(motion::angleInterpSpeed::Request &req,
+                                     motion::angleInterpSpeed::Response &res)
+{
+  return true;
 }
 
 bool Joint_Control::setAngles(motion::setAngles::Request &req,
--- edante/motion/src/joint_control.h
+++ edante/motion/src/joint_control.h
@@ -12,6 +12,10 @@
 
 #include <ros/ros.h>
 
+#include "motion/float32List.h"
+
+#include "motion/angleInterp.h"
+#include "motion/angleInterpSpeed.h"
 #include "motion/setAngles.h"
 #include "motion/changeAngles.h"
 #include "motion/getAngles.h"
@@ -34,15 +38,26 @@ public:
  // ROS publishers
 
  // ROS services
- bool setAngles(motion::setAngles::Request &req, motion::setAngles::Response &res);
- bool changeAngles(motion::changeAngles::Request &req, motion::changeAngles::Response &res);
- bool getAngles(motion::getAngles::Request &req, motion::getAngles::Response &res);
- bool closeHand(motion::useHand::Request &req, motion::useHand::Response &res);
- bool openHand(motion::useHand::Request &req, motion::useHand::Response &res);
+ bool angleInterp(motion::angleInterp::Request &req,
+                  motion::angleInterp::Response &res);
+ bool angleInterpSpeed(motion::angleInterpSpeed::Request &req,
+                       motion::angleInterpSpeed::Response &res);
+ bool setAngles(motion::setAngles::Request &req,
+                motion::setAngles::Response &res);
+ bool changeAngles(motion::changeAngles::Request &req,
+                   motion::changeAngles::Response &res);
+ bool getAngles(motion::getAngles::Request &req,
+                motion::getAngles::Response &res);
+ bool closeHand(motion::useHand::Request &req,
+                motion::useHand::Response &res);
+ bool openHand(motion::useHand::Request &req,
+               motion::useHand::Response &res);
 
 private:
  // ROS
  ros::NodeHandle* nh_;
+ ros::ServiceServer srv_angle_interp_;
+ ros::ServiceServer srv_angle_interp_speed_;
  ros::ServiceServer srv_set_angles_;
  ros::ServiceServer srv_change_angles_;
  ros::ServiceServer srv_get_angles_;
