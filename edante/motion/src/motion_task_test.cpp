05e9a4d3a81f21d82ba632d3e09726e25f89faa1
GreatAlexander <alex.bordallo@gmail.com>
2015-04-26 16:43:29 +0000

    Fixed motion_task_test.cpp to instantiate class for testing

--- edante/motion/src/motion_task_test.cpp
+++ edante/motion/src/motion_task_test.cpp
@@ -1,8 +1,19 @@
 #include <ros/ros.h>
+#include "motion_task.h"
 #include "definitions.h"
-
 
 int main(int argc, char *argv[]){
 
+  using namespace std;
+  ros::init(argc, argv, "motion_task");
+  Motion_Task* MotionTest = new Motion_Task();
+
+  ros::Rate r(10);
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    r.sleep();
+  }
   
 }
