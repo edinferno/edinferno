947b31991e023ffa698d5f41ce17b5ccae6a11dc
beppe712 <beppe712@hotmail.it>
2015-04-30 00:32:33 +0000

    Added all services for cartesian control   -Changed type of vectors of arrays into AL::ALValue   -Use of floatList for AL::ALValue arguments

--- edante/motion/src/cartesian_control.h
+++ edante/motion/src/cartesian_control.h
@@ -5,7 +5,15 @@
 #include <std_msgs/Bool.h>
 #include <vector>
 
+#include "motion/positionInterpolation.h"
+#include "motion/positionInterpolations.h"
+#include "motion/setPosition.h"
+#include "motion/changePosition.h"
 #include "motion/getPosition.h"
+#include "motion/transformInterpolation.h"
+#include "motion/transformInterpolations.h"
+#include "motion/setTransform.h"
+#include "motion/changeTransform.h"
 #include "motion/getTransform.h"
 
 #include <alproxies/almotionproxy.h>
@@ -20,8 +28,24 @@ public:
  ~Cartesian_Control();
 
  // ROS services
+ bool positionInterpolation(motion::positionInterpolation::Request &req,
+  motion::positionInterpolation::Response &res);
+ bool positionInterpolations(motion::positionInterpolations::Request &req,
+  motion::positionInterpolations::Response &res);
+ bool setPosition(motion::setPosition::Request &req,
+  motion::setPosition::Response &res);
+ bool changePosition(motion::changePosition::Request &req,
+  motion::changePosition::Response &res);
  bool getPosition(motion::getPosition::Request &req,
   motion::getPosition::Response &res);
+ bool transformInterpolation(motion::transformInterpolation::Request &req,
+  motion::transformInterpolation::Response &res);
+ bool transformInterpolations(motion::transformInterpolations::Request &req,
+  motion::transformInterpolations::Response &res);
+ bool setTransform(motion::setTransform::Request &req,
+  motion::setTransform::Response &res);
+ bool changeTransform(motion::changeTransform::Request &req,
+  motion::changeTransform::Response &res);
  bool getTransform(motion::getTransform::Request &req,
   motion::getTransform::Response &res);
 
@@ -29,7 +53,15 @@ public:
 private:
  // ROS
  ros::NodeHandle* nh_;
+ ros::ServiceServer srv_position_interpolation_;
+ ros::ServiceServer srv_position_interpolations_;
+ ros::ServiceServer srv_set_position_;
+ ros::ServiceServer srv_change_position_;
  ros::ServiceServer srv_get_position_;
+ ros::ServiceServer srv_transform_interpolation_;
+ ros::ServiceServer srv_transform_interpolations_;
+ ros::ServiceServer srv_set_transform_;
+ ros::ServiceServer srv_change_transform_;
  ros::ServiceServer srv_get_transform_;
 
  // NAOqi
