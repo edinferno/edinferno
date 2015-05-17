18603ea3ae811b4990b8ec34a49f99148fcc6977
Alejandro Bordallo <Alex.Bordallo@ed.ac.uk>
2015-05-17 20:15:37 +0000

    Implement createLedGroup, listGroups and listLEDs services

--- edante/signalling/src/led.cpp
+++ edante/signalling/src/led.cpp
@@ -13,10 +13,16 @@ Led::Led(ros::NodeHandle* nh) {
   nh_ = nh;
   leds = new AL::ALLedsProxy("127.0.0.1", 9559);
   INFO("Setting up LED signalling services" << std::endl);
+  srv_create_group_ = nh_->advertiseService("createLedGroup",
+                      &Led::createLedGroup, this);
   srv_led_angle_ = nh_->advertiseService("earLedsSetAngle",
                                          &Led::earLedsSetAngle, this);
   srv_fade_ = nh_->advertiseService("fade",
                                     &Led::fade, this);
+  srv_list_groups_ = nh_->advertiseService("listGroups",
+                     &Led::listGroups, this);
+  srv_list_leds_ = nh_->advertiseService("listLEDs",
+                                         &Led::listLEDs, this);
   srv_rotate_eyes_ = nh_->advertiseService("rotateEyes",
                      &Led::rotateEyes, this);
   srv_set_intensity_ = nh_->advertiseService("setIntensity",
@@ -24,6 +30,12 @@ Led::Led(ros::NodeHandle* nh) {
 }
 
 Led::~Led() {
+}
+
+bool Led::createLedGroup(signalling::createLedGroup::Request &req,
+                         signalling::createLedGroup::Response &res) {
+  leds->createGroup(req.groupName, req.ledNames);
+  return true;
 }
 
 bool Led::earLedsSetAngle(signalling::earLedsSetAngle::Request &req,
@@ -35,6 +47,18 @@ bool Led::earLedsSetAngle(signalling::earLedsSetAngle::Request &req,
 bool Led::fade(signalling::fade::Request &req,
                signalling::fade::Response &res) {
   leds->fade(req.name, req.intensity, req.duration);
+  return true;
+}
+
+bool Led::listGroups(signalling::getNames::Request &req,
+                     signalling::getNames::Response &res) {
+  res.names = leds->listGroups();
+  return true;
+}
+
+bool Led::listLEDs(signalling::getNames::Request &req,
+                   signalling::getNames::Response &res) {
+  res.names = leds->listLEDs();
   return true;
 }
 
--- edante/signalling/src/led.h
+++ edante/signalling/src/led.h
@@ -7,8 +7,10 @@
 
 #include <ros/ros.h>
 
+#include "signalling/createLedGroup.h"
 #include "signalling/earLedsSetAngle.h"
 #include "signalling/fade.h"
+#include "signalling/getNames.h"
 #include "signalling/rotateEyes.h"
 #include "signalling/setIntensity.h"
 
@@ -24,10 +26,16 @@ class Led {
   Led(ros::NodeHandle* nh);
   ~Led();
 
+  bool createLedGroup(signalling::createLedGroup::Request &req,
+                      signalling::createLedGroup::Response &res);
   bool earLedsSetAngle(signalling::earLedsSetAngle::Request &req,
                        signalling::earLedsSetAngle::Response &res);
   bool fade(signalling::fade::Request &req,
             signalling::fade::Response &res);
+  bool listGroups(signalling::getNames::Request &req,
+                  signalling::getNames::Response &res);
+  bool listLEDs(signalling::getNames::Request &req,
+                signalling::getNames::Response &res);
   bool rotateEyes(signalling::rotateEyes::Request &req,
                   signalling::rotateEyes::Response &res);
   bool setIntensity(signalling::setIntensity::Request &req,
@@ -36,8 +44,11 @@ class Led {
  private:
   // ROS
   ros::NodeHandle* nh_;
+  ros::ServiceServer srv_create_group_;
   ros::ServiceServer srv_led_angle_;
   ros::ServiceServer srv_fade_;
+  ros::ServiceServer srv_list_groups_;
+  ros::ServiceServer srv_list_leds_;
   ros::ServiceServer srv_rotate_eyes_;
   ros::ServiceServer srv_set_intensity_;
 
