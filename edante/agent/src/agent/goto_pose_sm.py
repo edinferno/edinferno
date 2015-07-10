1d51c19a821f876b42ef3bd429a8e5edb17e15fe
GreatAlexander <alex.bordallo@gmail.com>
2015-07-10 22:11:10 +0100

    Implement goto_pose state machine, used for reaching ready pose
    - Also made into independent goto_pose_sm smach, but I was unsuccessful
    into calling it from within ready_sm. (SMACH is weird)

--- edante/agent/src/agent/goto_pose_sm.py
+++ edante/agent/src/agent/goto_pose_sm.py
@@ -1,4 +1,4 @@
-# @file      ready_sm.py
+# @file      goto_pose_sm.py
 # @brief     Ready Game State smach
 # @author    Alejandro Bordallo <alex.bordallo@ed.ac.uk>
 # @date      2015-06-05
@@ -11,32 +11,23 @@ import smach_ros
 
 from actionlib import *
 from actionlib_msgs.msg import *
-from agent import *
-from params import Params
+# from params import *
 from motion_planning_msgs.msg import StandUpAction, StandUpGoal
 from navigation_msgs.msg import WalkToPoseAction, WalkToPoseGoal
 from navigation_msgs.msg import TurnToPoseAction, TurnToPoseGoal
 from navigation_msgs.msg import AlignToPoseAction, AlignToPoseGoal
 from geometry_msgs.msg import Pose2D
 
-class ReadySM(smach.StateMachine):
+class GotoPoseSM(smach.StateMachine):
     def __init__(self):
+        print(p.OPP_PENALTY)
         smach.StateMachine.__init__(self,outcomes=['succeeded','aborted', 'preempted'])
         with self:
-            p = Params()
-            # Stand up
-            smach.StateMachine.add('STAND_UP',
-                    smach_ros.SimpleActionState('motion_planning/stand_up',
-                        StandUpAction,
-                        goal = StandUpGoal(stand_up=True)),
-                   {'succeeded':'TURN_TO_POSE',
-                   'aborted':'STAND_UP',
-                   'preempted':'preempted'})
             # Turn towards target pose
             smach.StateMachine.add('TURN_TO_POSE',
                     smach_ros.SimpleActionState('navigation/turn_to_pose',
                         TurnToPoseAction,
-                        goal = TurnToPoseGoal(target_pose=p.OPP_PENALTY)),
+                        goal = TurnToPoseGoal(target_pose=OPP_PENALTY)),
                    {'succeeded':'WALK_TO_POSE',
                    'aborted':'STAND_UP',
                    'preempted':'preempted'})
@@ -44,7 +35,7 @@ class ReadySM(smach.StateMachine):
             smach.StateMachine.add('WALK_TO_POSE',
                     smach_ros.SimpleActionState('navigation/walk_to_pose',
                         WalkToPoseAction,
-                        goal = WalkToPoseGoal(target_pose=p.OPP_PENALTY)),
+                        goal = WalkToPoseGoal(target_pose=OPP_PENALTY)),
                    {'succeeded':'ALIGN_TO_POSE',
                    'aborted':'STAND_UP',
                    'preempted':'preempted'})
@@ -52,7 +43,15 @@ class ReadySM(smach.StateMachine):
             smach.StateMachine.add('ALIGN_TO_POSE',
                     smach_ros.SimpleActionState('navigation/align_to_pose',
                         AlignToPoseAction,
-                        goal = AlignToPoseGoal(target_pose=p.OPP_PENALTY)),
+                        goal = AlignToPoseGoal(target_pose=OPP_PENALTY)),
                    {'succeeded':'succeeded',
+                   'aborted':'STAND_UP',
+                   'preempted':'preempted'})
+            # Stand up
+            smach.StateMachine.add('STAND_UP',
+                    smach_ros.SimpleActionState('motion_planning/stand_up',
+                        StandUpAction,
+                        goal = StandUpGoal(stand_up=True)),
+                   {'succeeded':'TURN_TO_POSE',
                    'aborted':'STAND_UP',
                    'preempted':'preempted'})
