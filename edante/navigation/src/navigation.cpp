d96011d0ae4bb1267fc6ee1ca89e532bf5b260e7
GreatAlexander <alex.bordallo@gmail.com>
2015-07-10 22:05:21 +0100

    Implement Turn to pose action, which turns towards given pose

--- edante/navigation/src/navigation.cpp
+++ edante/navigation/src/navigation.cpp
@@ -1,4 +1,5 @@
 #include "navigation/move_to.hpp"
+#include "navigation/turn_to_pose.hpp"
 #include "navigation/walk_to_ball.hpp"
 #include "navigation/search_for_ball.hpp"
 
@@ -7,6 +8,7 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh("navigation");
   ROS_INFO("Running Navigation");
   MoveToAction move_to(nh, "move_to");
+  TurnToPoseAction turn_to_pose(nh, "turn_to_pose");
   WalkToBallAction walk_to_ball(nh, "walk_to_ball");
   SearchForBallAction search_for_ball(nh, "search_for_ball");
 
