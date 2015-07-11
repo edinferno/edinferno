cc938aa1e82285b8896e2b651a0f0694b12e0f90
Svetlin Penkov <s.v.penkov@sms.ed.ac.uk>
2015-07-11 18:37:16 +0100

    Edit the install script
    Copy the proper autoload.ini scripts

--- tools/install_edante.sh
+++ tools/install_edante.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-# Create config folder
+# Create config folders
 mkdir -p /home/nao/config/camera
+mkdir -p /home/nao/config/world
+
 # Add empty calibration files
 cp ./resources/top.yaml /home/nao/config/camera/
 cp ./resources/bottom.yaml /home/nao/config/camera/
 cp ./resources/pitch.yaml /home/nao/config/world/
 
 # Update autload.ini so that naoqi loads the camera module
-cp ./resources/autoload.ini /home/nao/naoqi/preferences/
+cp ./resources/nao_autoload.ini /home/nao/naoqi/preferences/autoload.ini
+sudo cp ./resources/sys_autoload.ini /etc/naoqi/preferences/autoload.ini
 
 # Add ros service
 sudo /resources/ros /etc/init.d/
 sudo rc-update add ros default
 
 # Update naoqi service
-cp ./resources/naoqi /etc/init.d/
+sudo cp ./resources/naoqi /etc/init.d/
