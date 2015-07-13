1f4b22492fd8baf025328e97bce96cf1b7784f9d
Svetlin Penkov <svepe@users.noreply.github.com>
2015-07-13 18:20:07 +0100

    Update install_edante.sh

--- tools/install_edante.sh
+++ tools/install_edante.sh
@@ -3,18 +3,20 @@
 # Create config folders
 mkdir -p /home/nao/config/camera
 mkdir -p /home/nao/config/world
+mkdir -p /home/nao/config/player
 
 # Add empty calibration files
 cp ./resources/top.yaml /home/nao/config/camera/
 cp ./resources/bottom.yaml /home/nao/config/camera/
 cp ./resources/pitch.yaml /home/nao/config/world/
+cp ./resources/player.yaml /home/nao/config/player/
 
 # Update autload.ini so that naoqi loads the camera module
 cp ./resources/nao_autoload.ini /home/nao/naoqi/preferences/autoload.ini
 sudo cp ./resources/sys_autoload.ini /etc/naoqi/autoload.ini
 
 # Add ros service
-sudo /resources/ros /etc/init.d/
+sudo cp /resources/ros /etc/init.d/
 sudo rc-update add ros default
 
 # Update naoqi service
