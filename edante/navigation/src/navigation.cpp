b57c6353aca912cb8c083cdf65b07fdc62d28d45
Alejandro Bordallo <alex.bordallo@gmail.com>
2015-07-17 05:03:36 +0100

    Implement new look at pose action, update related

--- edante/navigation/src/navigation.cpp
+++ edante/navigation/src/navigation.cpp
@@ -4,10 +4,13 @@
 #include "navigation/align_to_pose.hpp"
 #include "navigation/walk_to_ball.hpp"
 #include "navigation/search_for_ball.hpp"
+#include "navigation/look_at_pose.hpp"
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "navigation");
   ros::NodeHandle nh("navigation");
+  // ros::AsyncSpinner spinner(3);
+  // spinner.start();
   ROS_INFO("Running Navigation");
   MoveToAction move_to(nh, "move_to");
   TurnToPoseAction turn_to_pose(nh, "turn_to_pose");
@@ -15,9 +18,10 @@ int main(int argc, char** argv) {
   AlignToPoseAction align_to_pose(nh, "align_to_pose");
   WalkToBallAction walk_to_ball(nh, "walk_to_ball");
   SearchForBallAction search_for_ball(nh, "search_for_ball");
+  LookAtPoseAction look_at_pose(nh, "look_at_pose");
 
   ros::spin();
-
+  // ros::waitForShutdown();
   ros::shutdown();
 
   return 0;
